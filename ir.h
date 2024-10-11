5a728f6a62ede92b3ac96890323c544dddd74fcb
jce61 <jce61@uclive.ac.nz>
2024-10-12 00:32:32 +1300

    Update file ir.h

--- ir.h
+++ ir.h
@@ -1,6 +1,6 @@
 /** @file ir.h
-    @authors 
-    @date 
+    @authors Justin Cercado @jce61
+    @date 12/10/2024
     @brief header file for the IR communication module
 */
 
@@ -8,5 +8,11 @@
 #ifndef IR_H
 #define IR_H
 
+//initialise IR 
+void ir_init(void);
 
-#endif
+//Player move based on signal time(1 = rock, 2 = Paper, 3 = Scissors)
+int ir_get_signal(void);
+
+
+#endif //IR_H
